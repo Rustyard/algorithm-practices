5f64052799112b5018881daece163c8596ba4d67
Rustyard <42434369+Rustyard@users.noreply.github.com>
2021-04-23 12:34:24 +0800

    improved code, finishing TODO

--- Hello-World/RandomWord.java
+++ Hello-World/RandomWord.java
@@ -5,14 +5,12 @@ import edu.princeton.cs.algs4.StdRandom;
 public class RandomWord {
     public static void main(String[] args) {
         String championString = null;
+        String tempString = null;
         double i = 1.0;
         while (!StdIn.isEmpty()) {
+            tempString = StdIn.readString();
             if (StdRandom.bernoulli(1 / i)) {
-                championString = StdIn.readString();
-            }
-            else {
-                // TODO: RandomWord.java:14: Calling 'StdIn.readString()' in more than one place suggests poor design in this program. [Design]
-                StdIn.readString();
+                championString = tempString;
             }
             i++;
         }
