7abaa3f85148a1cc086b2e65fe1d17abd31da912
Rustyard <1092882207@qq.com>
2021-05-09 22:54:09 +0800

    implemented randomized queue

--- Stacks-and-Queues/Deque.java
+++ Stacks-and-Queues/Deque.java
@@ -2,133 +2,15 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 // double-linked list deque
-public class Deque<Item> implements Iterable<Item>{
+public class Deque<Item> implements Iterable<Item> {
     private Node first;
     private Node last;
     private int size;
-
-    private class Node {
-        Item item;
-        Node next;
-        Node prev;
-
-        Node() {
-            item = null;
-        }
-        Node(Item item) {
-            this.item = item;
-        }
-    }
-
-    @Override
-    public Iterator<Item> iterator() {
-        return new ListIterator();
-    }
-
-    private class ListIterator implements Iterator<Item>{
-        private Node current = first;
-
-        @Override
-        public boolean hasNext() {
-            return current != null;
-        }
-
-        @Override
-        public Item next() {
-            if (size == 0) {
-                throw new NoSuchElementException();
-            }
-            Item item = current.item;
-            current = current.next;
-            return item;
-        }
-
-        @Override
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
-    }
 
     public Deque() {
         first = null;
         last = null;
         size = 0;
-    }
-
-    public boolean isEmpty() {
-        return first == null;
-    }
-
-    public int size() {
-        return size;
-    }
-
-    public void addFirst(Item item) {
-        if (item == null) throw new IllegalArgumentException();
-
-        Node newNode = new Node(item);
-        if (isEmpty()) {
-            first = newNode;
-            last = newNode;
-            size = 1;
-        }
-        else {
-            Node oldFirst = first;
-            first = newNode;
-            first.next = oldFirst;
-            oldFirst.prev = first;
-            size++;
-        }
-    }
-
-    public void addLast(Item item) {
-        if (item == null) throw new IllegalArgumentException();
-
-        Node newNode = new Node(item);
-        if (isEmpty()) {
-            first = newNode;
-            last = newNode;
-            size = 1;
-        }
-        else {
-            Node oldLast = last;
-            last = newNode;
-            last.prev = oldLast;
-            oldLast.next = last;
-            size++;
-        }
-    }
-
-    public Item removeFirst() {
-        if (isEmpty()) throw new NoSuchElementException();
-        Item item = first.item;
-        if (first == last) {
-            first = null;
-            last = null;
-            size = 0;
-        }
-        else {
-            first = first.next;
-            first.prev = null;
-            size--;
-        }
-        return item;
-    }
-
-    public Item removeLast() {
-        if (isEmpty()) throw new NoSuchElementException();
-        Item item = last.item;
-        if (first == last) {
-            first = null;
-            last = null;
-            size = 0;
-        }
-        else {
-            last = last.prev;
-            last.next = null;
-            size--;
-        }
-        return item;
     }
 
     public static void main(String[] args) {
@@ -185,5 +67,120 @@ public class Deque<Item> implements Iterable<Item>{
             System.out.print(s + ",");
         }
         System.out.println("\n");
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void addFirst(Item item) {
+        if (item == null) throw new IllegalArgumentException();
+
+        Node newNode = new Node(item);
+        if (isEmpty()) {
+            first = newNode;
+            last = newNode;
+            size = 1;
+        } else {
+            Node oldFirst = first;
+            first = newNode;
+            first.next = oldFirst;
+            oldFirst.prev = first;
+            size++;
+        }
+    }
+
+    public void addLast(Item item) {
+        if (item == null) throw new IllegalArgumentException();
+
+        Node newNode = new Node(item);
+        if (isEmpty()) {
+            first = newNode;
+            last = newNode;
+            size = 1;
+        } else {
+            Node oldLast = last;
+            last = newNode;
+            last.prev = oldLast;
+            oldLast.next = last;
+            size++;
+        }
+    }
+
+    public Item removeFirst() {
+        if (isEmpty()) throw new NoSuchElementException();
+        Item item = first.item;
+        if (first == last) {
+            first = null;
+            last = null;
+            size = 0;
+        } else {
+            first = first.next;
+            first.prev = null;
+            size--;
+        }
+        return item;
+    }
+
+    public Item removeLast() {
+        if (isEmpty()) throw new NoSuchElementException();
+        Item item = last.item;
+        if (first == last) {
+            first = null;
+            last = null;
+            size = 0;
+        } else {
+            last = last.prev;
+            last.next = null;
+            size--;
+        }
+        return item;
+    }
+
+    private class Node {
+        Item item;
+        Node next;
+        Node prev;
+
+        Node() {
+            item = null;
+        }
+
+        Node(Item item) {
+            this.item = item;
+        }
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Item next() {
+            if (size == 0) {
+                throw new NoSuchElementException();
+            }
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
     }
 }
--- Stacks-and-Queues/RandomizedQueue.java
+++ Stacks-and-Queues/RandomizedQueue.java
@@ -1,38 +1,57 @@
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Iterator;
 
+// randomized queue implemented using array
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Item[] items;
+    private int size; // functioning as size and stack top at the same time
 
+    public RandomizedQueue() {
+        items = (Item[]) new Object[1];
+        size = 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
 
     @Override
     public Iterator<Item> iterator() {
         return null;
     }
 
-    public RandomizedQueue() {
-
-    }
-
     public boolean isEmpty() {
-        return true;
+        return size == 0;
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 
     public void enqueue(Item item) {
-
+        if (size == items.length) {
+            resize(2 * items.length);
+        }
+        items[size++] = item;
     }
 
     public Item dequeue() {
-        return null;
+        int randomIndex = StdRandom.uniform(0, size);
+        Item returnItem = items[randomIndex];
+        items[randomIndex] = items[--size]; // swap stack top
+        items[size] = null;
+        return returnItem;
+    }
+
+    private void resize(int sizeResize) {
+        Item[] newArray = (Item[]) new Object[sizeResize];
+        if (size >= 0) System.arraycopy(items, 0, newArray, 0, size);
+        items = newArray;
     }
 
     public Item sample() {
-        return null;
-    }
-
-    public static void main(String[] args) {
-
+        int randomIndex = StdRandom.uniform(0, size);
+        return items[randomIndex];
     }
 }
